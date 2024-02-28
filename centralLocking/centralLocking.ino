3d2b0021460fcd2ec0e8d9fcd8c3e9f5ce786125
Tensor Elmikawy <physmath99@gmail.com>
2024-02-28 12:54:13 -0700

    development done
    code and circuit are now ready for implementation

--- centralLocking/centralLocking.ino
+++ centralLocking/centralLocking.ino
@@ -1,25 +1,38 @@
 #include <Adafruit_Fingerprint.h>
+#include <HardwareSerial.h>
 
-//for xiao (yellowTx>7&BrownRx>6) 
-#define mySerial Serial1
-Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial); 
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&Serial1); //Serial1 on xiao(7,6) Serial0 on xiaoesp32c3(7,6) and Serial2 on esp32(16,17)
 
-// relay for ignition
-int rIgnition= 4;
 //relays for central locking
-int rFPS= 3; 
-int rGn= 2;
-int rSw= 1;
-int rGnLine= 0;
+int rGnln= D0; //a1
+int rGn= D1; //a2
+int rSwln= D2; //a3
+int rSw= D3; //a4
+int rFPS= D4; // b1
+bool rFPSstate= HIGH;
+// relay for disarm
+int rDisarm= D5; //b2
+// relays for ignition
+int rIgnition= D8; //b3
+int rBrake= D9; //b4
 
+///////////////////////   setup     ///////////////////////////////////
 void setup() {
-  pinMode(rIgnition, OUTPUT);  digitalWrite(rIgnition, LOW);
-  pinMode(rFPS, OUTPUT); digitalWrite(rFPS, LOW);
+  //outputs for central locking
+  pinMode(rGnln, OUTPUT); digitalWrite(rGnln, LOW);
   pinMode(rGn, OUTPUT); digitalWrite(rGn, LOW);
+  pinMode(rSwln, OUTPUT); digitalWrite(rSwln, LOW);
   pinMode(rSw, OUTPUT); digitalWrite(rSw, LOW);
-  pinMode(rGnLine, OUTPUT);  digitalWrite(rGnLine, LOW);
-  Serial1.begin(9600); //for xiao
-  while (!Serial); delay(100);
+  pinMode(rFPS, OUTPUT); digitalWrite(rFPS, rFPSstate);
+  //outputs for disarm
+  pinMode(rDisarm, OUTPUT); digitalWrite(rDisarm, HIGH);
+  //outputs for ignition
+  pinMode(rIgnition, OUTPUT); digitalWrite(rIgnition, LOW);
+  pinMode(rBrake, OUTPUT); digitalWrite(rBrake, LOW);
+  Serial1.begin(9600);
+  while (!Serial)
+    ; 
+  delay(100); //When the serial port is opened, the program starts to execute.
   Serial.println("\n\nAdafruit finger detect test");
   finger.begin(57600); delay(5);
   if (finger.verifyPassword()) { 
@@ -49,11 +62,13 @@ void setup() {
   } 
 }
 
+///////////////////////   loop     ///////////////////////////////////
 void loop() {
   getFingerprintID(); 
   delay(50); //no need for full speed
 }
 
+///////////////////////   getFingerprintID     ///////////////////////////////////
 uint8_t getFingerprintID() {    //byte = uint8_t = unsigned char
   uint8_t p = finger.getImage();
   switch (p) {
@@ -98,10 +113,78 @@ uint8_t getFingerprintID() {    //byte = uint8_t = unsigned char
   p = finger.fingerSearch();
   if (p == FINGERPRINT_OK) { 
     Serial.println("Found a print match!"); 
-  } 
+    Serial.print(">>>>> ID "); Serial.print(finger.fingerID); Serial.print(", >>>>> confidence "); Serial.println(finger.confidence);
+    // Tensor and Carie have six fingerprints stored on the fingerprint sensor. Their fingers 1 and 2 have full access to toggle central locking, disarm and ignition.
+    if (finger.fingerID >= 1 && finger.fingerID <= 2) { 
+      finger.LEDcontrol(FINGERPRINT_LED_BREATHING, 20, FINGERPRINT_LED_BLUE, 1); // blue led indicates full access granted.
+      rFPSstate = digitalRead(rFPS);
+      // Toggle central locking, disarm, and ignition into "ON" status
+      if (rFPSstate == LOW) {
+        digitalWrite(rGnln, HIGH); digitalWrite(rGn, HIGH); digitalWrite(rSwln, HIGH); digitalWrite(rSw, HIGH); Serial.print("wires cut. ");
+        delay(500);
+        digitalWrite(rFPS,HIGH); digitalWrite(rDisarm,HIGH); Serial.print("Disarm and FPS on. ");  
+        digitalWrite(rGnln, LOW); digitalWrite(rGn, LOW); digitalWrite(rSwln, LOW); digitalWrite(rSw, LOW); Serial.print("wires connected. "); 
+        delay(10);
+        digitalWrite(rIgnition, HIGH); delay(100); digitalWrite(rIgnition, LOW); delay(100);
+        digitalWrite(rIgnition, HIGH); delay(100); digitalWrite(rIgnition, LOW); delay(100);
+        digitalWrite(rBrake, HIGH); delay(10); digitalWrite(rIgnition, HIGH); delay(100); digitalWrite(rIgnition, LOW); digitalWrite(rBrake, LOW);
+        Serial.println("Ignition.");
+      }
+      // Toggle central locking and disarm into "OFF" status
+      else if (rFPSstate == HIGH) {
+        digitalWrite(rGnln, HIGH); digitalWrite(rGn, HIGH); digitalWrite(rSwln, HIGH); digitalWrite(rSw, HIGH); Serial.print("wires cut. ");
+        delay(500);
+        digitalWrite(rFPS,LOW); digitalWrite(rDisarm,LOW); Serial.print("Disarm and FPS off. ");  
+        digitalWrite(rGnln, LOW); digitalWrite(rGn, LOW); digitalWrite(rSwln, LOW); digitalWrite(rSw, LOW); Serial.println("wires connected."); 
+        delay(10);
+      }
+   }
+    // Tensor and Carie have six fingerprints stored on the fingerprint sensor. Their fingers 3 to 4 can toggle the central locking and disarm.
+    if (finger.fingerID >= 3 && finger.fingerID <= 4) { 
+      finger.LEDcontrol(FINGERPRINT_LED_BREATHING, 40, FINGERPRINT_LED_GREEN, 1); // blue indicates full access granted.
+      rFPSstate = digitalRead(rFPS);
+      // Toggle central locking and disarm into "ON" status
+      if (rFPSstate == LOW) {
+        digitalWrite(rGnln, HIGH); digitalWrite(rGn, HIGH); digitalWrite(rSwln, HIGH); digitalWrite(rSw, HIGH); Serial.print("wires cut. ");
+        delay(500);
+        digitalWrite(rFPS,HIGH); digitalWrite(rDisarm,HIGH); Serial.print("Disarm and FPS on. ");  
+        digitalWrite(rGnln, LOW); digitalWrite(rGn, LOW); digitalWrite(rSwln, LOW); digitalWrite(rSw, LOW); Serial.print("wires connected. "); 
+        delay(10);
+      }
+      // Toggle central locking and disarm into "OFF" status
+      else if (rFPSstate == HIGH) {
+        digitalWrite(rGnln, HIGH); digitalWrite(rGn, HIGH); digitalWrite(rSwln, HIGH); digitalWrite(rSw, HIGH); Serial.print("wires cut. ");
+        delay(500);
+        digitalWrite(rFPS,LOW); digitalWrite(rDisarm,LOW); Serial.print("Disarm and FPS off. ");  
+        digitalWrite(rGnln, LOW); digitalWrite(rGn, LOW); digitalWrite(rSwln, LOW); digitalWrite(rSw, LOW); Serial.println("wires connected."); 
+        delay(10);
+      }
+    }
+    // all other fingerprint stored on the fingerprint sensor are only allowed to toggle the central locking.
+    else { 
+      finger.LEDcontrol(FINGERPRINT_LED_BREATHING, 50, FINGERPRINT_LED_PURPLE, 1); // purple indiicates access granted to toggle the central locking.
+      rFPSstate = digitalRead(rFPS);
+      // Toggle central locking into "ON" status
+      if (rFPSstate == LOW) {
+        digitalWrite(rGnln, HIGH); digitalWrite(rGn, HIGH); digitalWrite(rSwln, HIGH); digitalWrite(rSw, HIGH); Serial.print("wires cut. ");
+        delay(500);
+        digitalWrite(rFPS,HIGH); Serial.print("FPS on. ");  
+        digitalWrite(rGnln, LOW); digitalWrite(rGn, LOW); digitalWrite(rSwln, LOW); digitalWrite(rSw, LOW); Serial.print("wires connected. "); 
+        delay(10);
+      }
+      // Toggle central locking into "OFF" status
+      else if (rFPSstate == HIGH) {
+        digitalWrite(rGnln, HIGH); digitalWrite(rGn, HIGH); digitalWrite(rSwln, HIGH); digitalWrite(rSw, HIGH); Serial.print("wires cut. ");
+        delay(500);
+        digitalWrite(rFPS,LOW); Serial.print("FPS off. ");  
+        digitalWrite(rGnln, LOW); digitalWrite(rGn, LOW); digitalWrite(rSwln, LOW); digitalWrite(rSw, LOW); Serial.println("wires connected."); 
+        delay(10);
+      }
+    }
+  }
   else if (p == FINGERPRINT_PACKETRECIEVEERR) { 
     Serial.println("Communication error"); 
-    //the finger.LEDcontrol() function takes these parameters (control, speed, coloridx, count)
+    //the finger.LEDcontrol() function takes these parameters (control, speed, coloridx, count)  YELLOW CYAN COLDWHITE WARMWHITE
     finger.LEDcontrol(FINGERPRINT_LED_FLASHING, 25, FINGERPRINT_LED_RED, 3); // try again, this time use your finger!
     return p; 
   } 
@@ -115,32 +198,11 @@ uint8_t getFingerprintID() {    //byte = uint8_t = unsigned char
     finger.LEDcontrol(FINGERPRINT_LED_FLASHING, 25, FINGERPRINT_LED_RED, 3);// the univerese, god, buddha or OMG particle has intervined.
     return p; 
   } 
-  // found a match
-  Serial.print(" ID "); Serial.print(finger.fingerID);
-  Serial.print(", confidence "); Serial.println(finger.confidence);
-  if (finger.fingerID == 1 ) { // Tensor has two fingerprints stored on the fingerprint sensor and has full access to toggle the central locking and toggle the ignition.
-    finger.LEDcontrol(FINGERPRINT_LED_BREATHING, 15, FINGERPRINT_LED_BLUE, 1); // blue indicates full access granted.
-    //toggling the central locking
-    digitalWrite(rGn, HIGH); digitalWrite(rSw, HIGH); digitalWrite(rGnLine, HIGH); Serial.print("3 relays on, ");
-    delay(150);
-    digitalWrite(rFPS,!digitalRead(rFPS)); Serial.print("FPS relay toggled, ");    //int rFPSToggled = 1-rFPSStatus; digitalWrite(rFPS, rFPSToggled); 
-    delay(150);
-    digitalWrite(rGn, LOW); digitalWrite(rSw, LOW); digitalWrite(rGnLine, LOW); Serial.println("3 relays off.");
-    //toggling the ignition   
-    digitalWrite(rIgnition,!digitalRead(rIgnition)); Serial.println("Ignition relay toggled.");
-  }
-  if (finger.fingerID == 3 ) { // Carie has one fingerprint stored on the fingerprint sensor and is only allowed to toggle the central locking.
-    finger.LEDcontrol(FINGERPRINT_LED_BREATHING, 50, FINGERPRINT_LED_PURPLE, 1); // purple indiicates access granted to toggle the central locking.
-    //toggling the central locking
-    digitalWrite(rGn, HIGH); digitalWrite(rSw, HIGH); digitalWrite(rGnLine, HIGH); Serial.print("3 relays on, ");
-    delay(150);
-    digitalWrite(rFPS,!digitalRead(rFPS)); Serial.print("FPS relay toggled, ");    
-    delay(150);
-    digitalWrite(rGn, LOW); digitalWrite(rSw, LOW); digitalWrite(rGnLine, LOW); Serial.println("3 relays off.");
-  }
   return finger.fingerID; 
+  Serial.println("----------------------    hmmmmmm    ---------------------------"); 
 }
 
+///////////////////////   getFingerprintIDez     ///////////////////////////////////
 // returns "-1" when fail or "ID" when success
 int getFingerprintIDez() {   
   uint8_t p = finger.getImage();
